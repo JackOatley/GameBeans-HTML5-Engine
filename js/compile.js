9b44595f616cfe8fbcf7095d4b27ca425234e056
JackOatley <jack.f.oatley@gmail.com>
2018-06-29 09:26:43 +0100

    Action expression can now add strings together.

--- js/compile.js
+++ js/compile.js
@@ -19,9 +19,8 @@ export default class Compiler {
 		if (!isNaN(Number(exp)))
 			return Number(exp);
 		
-		if (Compiler.isBoolean(exp)) {
+		if (Compiler.isBoolean(exp))
 			return exp === true || exp === "true";
-		}
 		
 		if (Compiler.isFunction(exp)
 		||  Compiler.isOperator(exp)
@@ -29,91 +28,98 @@ export default class Compiler {
 		||  Compiler.isCss(exp))
 			return exp;
 		
-		if (Compiler.isString(exp)) {
-			return exp.slice(1, -1);
-		}
-		
 		if (typeof exp === "string") {
-		
-			let n,
-				string = "",
-				sIndex = -1,
-				newArr = [];
-				
-			for (n = 0; n < exp.length; n++) {
-				
-				let c = exp.charAt(n);
-				switch (c) {
-					
-					case ("'"):
-						if (sIndex === -1) {
-							sIndex = n + 1
-						} else {
-							newArr.push("'" + exp.slice(sIndex, n) + "'");
-							sIndex = -1;
-						}
-						break;
-						
-					default:
-						if (sIndex === -1 && c !== " ") {
-							if (Compiler.splitCharacters.includes(c)) {
-								if (string !== "") {
-									newArr.push(string);
-									string = "";
-								}
-								newArr.push(c);
-							} else {
-								string += c;
-							}
-						} else {
-							string += c;
-						}
-						
-				}
-				
-			}
-			
-			if (string !== "") {
-				newArr.push(string);
-				string = "";
-			}
-			
-			let y = "";
-			
-			newArr.forEach((word) => {
-				let num = parseInt(word);
-				if (isNaN(num)) {
-					let c = word.charAt(0);
-					if (c === "'") {
-						y += word;
-					} else if (Compiler.splitCharacters.includes(c)) {
-						y += word;
-					} else {
-						if (word === "fps")
-							y += global.fps;
-						else {
-							if (!Compiler.isResource(word)
-							&&  Compiler.isSingleWord(word)
-							&&  !Compiler.isString(word)
-							&&  !Compiler.isArray(word)) {
-								y += "this." + word;
-								//this[word] = this[word] || word;
-							} else {
-								y += word;
-							}
-						}
-					}
-				} else {
-					y += word;
-				}
-			});
-			
-			//console.log(y);
-			return eval(y);
+			let words = Compiler.getWords(exp);
+			let expression = Compiler.joinWords(words);
+			return eval(expression);
 		}
 		
 		return exp;
 
+	}
+	
+	/**
+	 *
+	 */
+	static getWords(exp) {
+		let n;
+		let string = "";
+		let sIndex = -1;
+		let sType = "";
+		let newArr = [];
+			
+		for (n = 0; n < exp.length; n++) {
+			
+			let c = exp.charAt(n);
+			
+			if (c === "'" || c === "\"") {
+				if (sIndex === -1) {
+					sType = c;
+					sIndex = n + 1;
+				} else if (sType === c) {
+					newArr.push("'" + exp.slice(sIndex, n) + "'");
+					sIndex = -1;
+				}
+			}
+				
+			else if (sIndex === -1) {
+				if (c !== " ") {
+					if (Compiler.splitCharacters.includes(c)) {
+						if (string !== "") {
+							newArr.push(string);
+							string = "";
+						}
+						newArr.push(c);
+					} else {
+						string += c;
+					}
+				} else {
+					string += c;
+				}
+			}
+			
+		}
+		
+		if (string !== "") {
+			newArr.push(string);
+			string = "";
+		}
+		
+		return newArr;
+	}
+	
+	/**
+	 *
+	 */
+	static joinWords(words) {
+		let y = "";
+		words.forEach((word) => {
+			let num = parseInt(word);
+			if (isNaN(num)) {
+				let c = word.charAt(0);
+				if (c === "'" || c === " ") {
+					y += word;
+				} else if (Compiler.splitCharacters.includes(c)) {
+					y += word;
+				} else {
+					if (word === "fps")
+						y += global.fps;
+					else {
+						if (!Compiler.isResource(word)
+						&&  Compiler.isSingleWord(word)
+						&&  !Compiler.isString(word)
+						&&  !Compiler.isArray(word)) {
+							y += "this." + word;
+						} else {
+							y += word;
+						}
+					}
+				}
+			} else {
+				y += word;
+			}
+		});
+		return y;
 	}
 	
 	/** */
